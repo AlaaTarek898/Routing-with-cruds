009fbb3fe30d745d53262918478858cdbee71440
Alaa Tarek <Alaa.Tarek@Intermark-corp.local>
2025-07-21 16:32:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import { BrowserRouter, Routes, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
+
+import './App.css'
+import Layout from './Layout/Layout'
+import Home from './pages/Home/Home' 
+import About from './pages/About/About';
+import Portofolio from './pages/Portofolio/Portofolio';
+import Contact from './pages/Contact/Contact';
+
+ function App() {
+ const routes=createBrowserRouter([ 
+    {path: "/",element:<Layout/> , children:[  
+      {path: "/home",element:<Home/> },
+      {path: "/about",element:<About/> },
+      {path: "/portofolio",element:<Portofolio/> },
+      {path: "/contact",element:<Contact/> },] },
+
+    ],)
+  
+  
+
+  return (
+    <RouterProvider router={routes}/>
+
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+//style
+import styles from'./Home.module.css'
+//assets
+import person1 from '../../assets/person.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+  return (
+  <div className={styles.wrapper} >
+  <img className='w-25'  src={person1} alt='person1'/>
+  <h1>start Framework</h1>
+  <div className='d-flex w-20 align-items-center justify-content-around mt-2 mb-2'>
+    <div className={styles.separator}></div>
+    <FontAwesomeIcon icon={faStar} />
+    <div  className={styles.separator}></div>
+  </div>
+  <p>Graphic Artist - Web Designer - Illustrator</p>
+  </div>
+  )
+}
