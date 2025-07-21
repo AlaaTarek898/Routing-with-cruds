269657f1a5751860580fb970f8aa591b2d0379b5
Alaa Tarek <Alaa.Tarek@Intermark-corp.local>
2025-07-21 16:45:25 +0300

    fix: corrected Layout import path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router";
 
 import './App.css'
-import Layout from './Layout/Layout'
+import Layout from './pages/Layout/Layout'
 import Home from './pages/Home/Home' 
 import About from './pages/About/About';
 import Portofolio from './pages/Portofolio/Portofolio';
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 
 
-import NavBar from '../../../components/Nav/NavBar'
-import Footer from '../../../components/Footer/Footer'
+import NavBar from '../../components/Nav/NavBar'
+import Footer from '../../components/Footer/Footer'
 
 
 export default function Layout() {
