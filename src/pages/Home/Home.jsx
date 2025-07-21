ac21d58e797a5d05e0472ee520d50c7e060ecf61
Alaa Tarek <Alaa.Tarek@Intermark-corp.local>
2025-07-22 00:46:00 +0300

    deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-  <div className={styles.wrapper} style={{ backgroundColor: 'red' }}>
+  <div className={styles.wrapper} >
 
   <img className='w-25'  src={person1} alt='person1'/>
   <h1>Start Framework ✅</h1>
